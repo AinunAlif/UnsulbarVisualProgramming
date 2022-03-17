5dde49e3f6f2767998e9999b3cbc42bfeb010429
AinunAlif <100109044+AinunAlif@users.noreply.github.com>
2022-03-16 18:52:15 -1200

    Add files via upload

---
+++ Pass.py
@@ -0,0 +1,13 @@
+class batu:
+  pass
+   
+batu1 = batu()
+batu2 = batu();
+ 
+batu1.warna = 'putih'
+batu1.ukuran = 'kecil'
+ 
+batu2.warna = 'hitam'
+batu2.ukuran = 'besar'
+ 
+print(batu1.__dict__)
